e4ea5af8039cf8787187ec9a7c8e841a9cee008c
Brandon S <deftones2001@gmail.com>
2018-01-15 03:23:23 -0800

    Did some of the implementation of the Rank enum. Will likely add more and possibly change some things around.
    Signed-off-by: Brandon S <deftones2001@gmail.com>

--- src/Main/Rank.java
+++ src/Main/Rank.java
@@ -2,5 +2,35 @@ package Main;
 
 public enum Rank
 {
+    DEUCE(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7),
+    EIGHT(8), NINE(9), TEN(10), JACK(12), QUEEN(13), KING(14),
+    ACE(15), JOKER(0);
 
+    private int cardRank;
+
+    Rank(final int cardRank)
+    {
+        this.cardRank = cardRank;
+    }
+
+    /**
+     * Gets the rank of the card.
+     *
+     * @return the card's rank
+     */
+    public int getCardRank()
+    {
+        return this.cardRank;
+    }
+
+    /**
+     * Sets the joker's rank when used in straights
+     * or for ace high.
+     *
+     * @param newRank - the joker's new rank.
+     */
+    public void setJokerRank(final int newRank)
+    {
+        this.cardRank = newRank;
+    }
 }
